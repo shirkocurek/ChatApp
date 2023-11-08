69c4dacd46553d18b1e525bbede3792e5b83f7d6
shirko <shir617@gmail.com>
2023-11-08 18:55:42 -0500

    dark theme added

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <Header />
           {children}
         </ThemeProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="">
-<h1>Lets build saas</h1>
+      <h1>Lets build saas</h1>
     </main>
-  )
+  );
 }
