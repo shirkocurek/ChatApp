dca6f98a3111ab48ef306b355b7594dd1b61b443
shirko <shir617@gmail.com>
2023-11-21 11:50:21 -0500

    main page style update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import DemoGif from "@/images/landingPage/demo.png";
+import { ArrowLongRightIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
@@ -10,11 +12,11 @@ export default function Home() {
           aria-hidden="true"
         >
           <div
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0. 1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0. 1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
           />
         </div>
         <div className="py-12 sm:py-20 lg:pb-40">
@@ -32,15 +34,18 @@ export default function Home() {
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <Link
                   href="/chat"
-                  className="rounded-md bg-indigo-600 px-3.5 py 2.5 text-sm font-semibold text-white dark:text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white dark:text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get Started
                 </Link>
                 <Link
                   href="/pricing"
-                  className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-300"
+                  className="text-sm font-semibold flex leading-6 text-gray-900 dark:text-gray-300"
                 >
-                  View Pricing <span aria-hidden="true"></span>
+                  View Pricing
+                  <span aria-hidden="true">
+                    <ArrowLongRightIcon className="h-6 w-8" />
+                  </span>
                 </Link>
               </div>
             </div>
@@ -48,16 +53,28 @@ export default function Home() {
             <div className="mt-16 flow-root sm:mt-24">
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                 <Image
-                unoptimized
-                src={'encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5OC_B3gY5NGfbAuZ10jLOA9xEFYypNiOADw&usqp=CAU'}
-                alt='App screenshot'
-                width={2432}
-                height={1442}
-                className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                  unoptimized
+                  src={DemoGif}
+                  alt="App screenshot"
+                  width={2432}
+                  height={1442}
+                  className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
                 />
               </div>
             </div>
           </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0. 1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
         </div>
       </div>
     </main>
